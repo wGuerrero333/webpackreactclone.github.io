8e32814a335ca5c43ecd578e863fda6cbb87c506
AndyGeek <andy.eulogio@gmail.com>
2020-09-18 19:36:49 -0500

    Create a project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import './styles/index.css'
+
+var div = document.createElement('div')
+var text = document.createTextNode('AndyGeek')
+div.appendChild(text)
+document.body.appendChild(div)
+
+console.log("Gaaa");
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './styles/[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'React app'
+        })
+    ]
+}
