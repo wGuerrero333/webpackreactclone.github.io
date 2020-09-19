49d7f337693d44f215174aa9cafbc3f575695697
AndyGeek <andy.eulogio@gmail.com>
2020-09-18 20:25:53 -0500

    Add Babel in project

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './styles/[name].css'
-        }),
-        new HtmlWebpackPlugin({
-            title: 'React app'
-        })
-    ]
-}
+  entry: "./src/app.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./styles/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "React app",
+    }),
+  ],
+};
